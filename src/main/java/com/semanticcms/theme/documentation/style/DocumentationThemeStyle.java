a17ddc057cb74fee25d1a1439d641d6f4c101bf4
AO Industries, Inc <support@aoindustries.com>
2022-04-16 02:02:49 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- src/main/java/com/semanticcms/theme/documentation/style/DocumentationThemeStyle.java
+++ src/main/java/com/semanticcms/theme/documentation/style/DocumentationThemeStyle.java
@@ -1,6 +1,6 @@
 /*
  * semanticcms-theme-documentation-style - Default style for SemanticCMS theme tailored for technical documentation.
- * Copyright (C) 2020, 2021  AO Industries, Inc.
+ * Copyright (C) 2020, 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with semanticcms-theme-documentation-style.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.semanticcms.theme.documentation.style;
 
 import com.aoapps.web.resources.registry.Group;
