7e761c128f52a277ddcb9635b22ddbb02ac7aa86
AO Industries, Inc <support@aoindustries.com>
2022-04-24 14:43:15 -0500

    Performed automatic source code clean-up with rewrite-maven-plugin

--- book/src/main/java/com/semanticcms/theme/documentation/style/book/ModularApidocsRedirect.java
+++ book/src/main/java/com/semanticcms/theme/documentation/style/book/ModularApidocsRedirect.java
@@ -41,9 +41,9 @@ public class ModularApidocsRedirect extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
     HttpServletUtil.sendRedirect(
-      HttpServletResponse.SC_MOVED_PERMANENTLY, req, resp,
-      "/theme-documentation/style/apidocs/com.semanticcms.theme.documentation.style/com" + Objects.toString(req.getPathInfo(), ""),
-      URIParametersUtils.of(req.getQueryString()), true, false
+        HttpServletResponse.SC_MOVED_PERMANENTLY, req, resp,
+        "/theme-documentation/style/apidocs/com.semanticcms.theme.documentation.style/com" + Objects.toString(req.getPathInfo(), ""),
+        URIParametersUtils.of(req.getQueryString()), true, false
     );
   }
 }
